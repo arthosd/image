18d6c75131191fb58c49cc67e1dfa754f3a1009f
arthosd <elie.debs@outlook.fr>
2021-05-18 23:58:59 +0200

    Dépot V1

--- main.cpp
+++ main.cpp
@@ -15,14 +15,18 @@ using namespace cv;
 int main()
 {
 
-    Image image = Image("/home/elie/Documents/Projet/Fac/Image/assets/jus1.jpeg");
+    Image image = Image("/home/elie/Documents/Projet/Fac/Image/assets/coca3.jpeg");
 
-    image.to_gray();       // Transformation en niveua de gris
+    image.to_gray(); // Transformation en niveua de gris
+    image.show("Image - gris");
     image.remove_noise(5); // On attenue l'image avec un filtre gaussien
 
     image.detect_edge(20, 120); // On detecte les contours du verre
+    image.show("Canny - filter");
 
     Mat proj = image.line_level(); // On les lignes de niveaux
+
+    imshow("Lignes de niveaux", proj);
 
     int verify = image.treat_histogram(proj);           // Récupération de la ligne de niveau qui nous interesse
     int houghline = image.hough_transform(100, verify); // Vérification de cette ligne de niveau avec les lignes de la transformée de hough
